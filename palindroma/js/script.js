2e02367a6d769e41cf9308e692c437a96c254222
Gozzissimo <fantini.nico@gmail.com>
2021-11-29 16:01:52 +0100

    palindromo con split-reverse-join

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,3 +2,13 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 //     (prima senza funzione e poi con funzione)
+
+const word = prompt('Inserisci una Parola');
+
+// console.log(word.split('').reverse().join(''));
+
+if (word.split('').reverse().join('') == word) {
+    console.log('la parola è palindroma');
+} else {
+    console.log('non è palindroma');
+}
